f8186995447b7fc187cc49f99fd90169687f2c8c
schismatiic <leonardojofre2003@gmail.com>
2026-05-12 00:43:16 -0400

    The nodes are linked and toString, append and prepend works

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,5 +1,37 @@
+import { Node } from "./Node.js";
 class LinkedList {
-  append(value) {}
-  prepend(value) {}
+  constructor(head = null) {
+    this.head = head;
+  }
+  append(value) {
+    if (this.head === null) {
+      return this.prepend(value);
+    } else {
+      let tmp = this.head;
+      while (tmp.nextNode !== null) tmp = tmp.nextNode;
+      tmp.nextNode = new Node(value, null);
+      return tmp.nextNode;
+    }
+  }
+  prepend(value) {
+    this.head = new Node(value, this.head);
+    return this.head;
+  }
+  returnHead() {
+    return this.head !== null ? this.head.value : undefined;
+  }
+  toString() {
+    let stringToPrint = "";
+    let tmp = this.head;
+    stringToPrint = stringToPrint + `( ${tmp.value} ) -> `;
+    while (tmp.nextNode !== null) {
+      tmp = tmp.nextNode;
+      stringToPrint = stringToPrint + `( ${tmp.value} ) -> `;
+      if (tmp.nextNode === null) {
+        stringToPrint = stringToPrint + `null`;
+      }
+    }
+    console.log(stringToPrint);
+  }
 }
 export { LinkedList };
--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import { LinkedList } from "./LinkedList.js";
 import { Node } from "./Node.js";
+
+const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.toString();
